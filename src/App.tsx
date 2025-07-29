51ccbb5c48c0ad1e106332e97c7e4d2f3977036b
rardan97 <risalardan@gmail.com>
2025-07-29 13:46:26 +0700

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Category from './pages/Category'
 import Product from './pages/Product'
 import Role from './pages/Role'
 import Petugas from './pages/Petugas'
+import Login from './pages/Login'
+import Register from './pages/Register'
 // import Page from './pages/Pages'
 
 function App() {
@@ -14,9 +16,12 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={
           <AppLayout />
         }>
+
           <Route index element={<Home />} />
           <Route path="/category" element={<Category />} />
           <Route path="/product" element={<Product />} />
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -18,48 +18,45 @@ import { Card } from "@/components/ui/card";
 
 const LayoutContent: React.FC = () => {
 
-    return (
-    
-
-        
+    return (  
         <SidebarProvider>
-    <AppSidebar />
-    <SidebarInset>
-        <header className="flex h-14 shrink-0 items-center gap-2">
-        <div className="flex flex-1 items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-            <BreadcrumbList>
-                <BreadcrumbItem>
-                <BreadcrumbPage className="line-clamp-1">
-                    Project Management & Task Tracking
-                </BreadcrumbPage>
-                </BreadcrumbItem>
-            </BreadcrumbList>
-            </Breadcrumb>
-        </div>
-        <div className="ml-auto px-3">
-            <NavActions />
-        </div>
-        </header>
-        <Card className="m-9 p-9">
-            <Outlet />
-        </Card>
-        
-        
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-14 shrink-0 items-center gap-2">
+                <div className="flex flex-1 items-center gap-2 px-3">
+                    <SidebarTrigger />
+                    <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                    />
+                    <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                        <BreadcrumbPage className="line-clamp-1">
+                            Project Management & Task Tracking
+                        </BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                    </Breadcrumb>
+                </div>
+                <div className="ml-auto px-3">
+                    <NavActions />
+                </div>
+                </header>
+                <Card className="m-9 p-9">
+                    <Outlet />
+                </Card>
+                
+                
 
-        
-        
-        {/* 
-        <div className="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl" />
-        <div className="bg-muted/50 mx-auto h-full w-full max-w-3xl rounded-xl" />
-        </div> */}
-    </SidebarInset>
-    </SidebarProvider>
+                
+                
+                {/* 
+                <div className="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl" />
+                <div className="bg-muted/50 mx-auto h-full w-full max-w-3xl rounded-xl" />
+                </div> */}
+            </SidebarInset>
+        </SidebarProvider>
     );
 };
 
